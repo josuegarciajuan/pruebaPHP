4f520e24a7c399a5de1825722b98cda526937d77
Your Name <you@example.com>
2019-01-17 22:12:53 +0100

    anyadida leyenda al calendario

--- resources/views/index/calendario.blade.php
+++ resources/views/index/calendario.blade.php
@@ -1,2 +1,29 @@
 <label for="sel1">Elige un día:</label>
 <div id='calendar'></div>
+
+
+
+<table class="leyenda">
+  <tr>
+	<td style="background-color:blue">
+		Seleccionado
+		</td>
+		<td style="background-color:silver">
+		Pasado
+		</td>
+		<td style="background-color:white">
+		Libre
+		</td>
+		<td style="background-color:black">
+		No hay ses.
+		</td>
+		<td style="background-color:red">
+		Completo
+		</td>
+	
+
+
+	</tr>
+</table>
+
+
